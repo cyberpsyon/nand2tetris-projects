768d300cd9bce59f86e373e7d63b507702ad276c
Ben Mickens <ben@cyberpsyon.com>
2025-07-06 22:38:01 -0500

    Added Fill.asm and Fill.hack files

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -7,5 +7,78 @@
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
+(LOOP)
+  @KBD
+  D=M
+  @FILL_BLACK
+  D;JNE    // If key is pressed, go fill black
 
-//// Replace this comment with your code.
+  @FILL_WHITE
+  0;JMP    // If no key, go fill white
+
+// Fill screen black
+(FILL_BLACK)
+  @SCREEN
+  D=A
+  @PTR
+  M=D
+
+  @8192
+  D=A
+  @COUNT
+  M=D
+
+(BLACK_LOOP)
+  @COUNT
+  D=M
+  @LOOP
+  D;JEQ
+
+  @PTR
+  A=M
+  M=-1
+
+  @PTR
+  M=M+1
+  @COUNT
+  M=M-1
+  @BLACK_LOOP
+  0;JMP
+
+// Fill screen white
+(FILL_WHITE)
+  @SCREEN
+  D=A
+  @PTR
+  M=D
+
+  @8192
+  D=A
+  @COUNT
+  M=D
+
+(WHITE_LOOP)
+  @COUNT
+  D=M
+  @LOOP
+  D;JEQ
+
+  @PTR
+  A=M
+  M=0
+
+  @PTR
+  M=M+1
+  @COUNT
+  M=M-1
+  @WHITE_LOOP
+  0;JMP
+
+
+
+
+
+
+
+
+
