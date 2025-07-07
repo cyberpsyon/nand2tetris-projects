fdd276bc78472e3e9a6260ec8259d9338aafac2e
Ben Mickens <ben@cyberpsyon.com>
2025-07-06 21:51:34 -0500

    Added Mult.asm and Mult.hack files

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,4 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@2
+M=0 // R2 = 0 
+
+@1
+D=M
+@R1
+M=D // Store R1 in R1 variable (RAM[17])
+
+(LOOP)
+  @R1
+  D=M
+  @END
+  D;JEQ // Exit if R1 == 0
+
+  @0
+  D=M
+  @2
+  M=M+D // R2 += R0
+
+  @R1
+  M=M-1 // R1--
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP // Inf loop to halt
