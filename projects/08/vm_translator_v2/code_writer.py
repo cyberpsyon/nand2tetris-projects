b4e07776bf5153ce26cfccf2ffdb00ab3c3fed64
Ben Mickens <ben@cyberpsyon.com>
2025-08-02 19:02:27 -0500

    Updated code in 3 programs

--- projects/08/vm_translator_v2/code_writer.py
+++ projects/08/vm_translator_v2/code_writer.py
@@ -1,142 +1,102 @@
+import os
 import re
 from parser import Parser
 
 class CodeWriter:
-    label_cnt = 0
-
-    def __init__(self, file_out):
-        self.arth_jump_flag = 0
-        self.out_printer = open(file_out, 'w')
-        self.file_name = self._strip_ext(file_out)
-
-    def _strip_ext(self, path):
-        return re.sub(r"\.[^.]+$", "", path.split("/")[-1])
+    def __init__(self, file_out_path):
+        try:
+            self.out_file = open(file_out_path, 'w')
+            self.file_name = ""
+            self.arth_jump_count = 0
+            self.return_label_count = 0
+        except IOError:
+            raise IOError(f"Could not open file for writing: {file_out_path}")
 
     def set_file_name(self, file_path):
-        self.file_name = self._strip_ext(file_path)
+        self.file_name = os.path.splitext(os.path.basename(file_path))[0]
+
+    def write_init(self):
+        self.out_file.write("@256\nD=A\n@SP\nM=D\n")
+        self.write_call("Sys.init", 0)
+
+    def write_label(self, label):
+        self.out_file.write(f"({label})\n")
+
+    def write_goto(self, label):
+        self.out_file.write(f"@{label}\n0;JMP\n")
+
+    def write_if(self, label):
+        self.out_file.write("@SP\nAM=M-1\nD=M\n" + f"@{label}\nD;JNE\n")
+
+    def write_call(self, function_name, num_args):
+        return_label = f"{function_name}$ret.{self.return_label_count}"
+        self.return_label_count += 1
+        self.out_file.write(f"@{return_label}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        for seg in ["LCL", "ARG", "THIS", "THAT"]:
+            self.out_file.write(f"@{seg}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        self.out_file.write(f"@SP\nD=M\n@5\nD=D-A\n@{num_args}\nD=D-A\n@ARG\nM=D\n")
+        self.out_file.write("@SP\nD=M\n@LCL\nM=D\n")
+        self.write_goto(function_name)
+        self.write_label(return_label)
+
+    def write_return(self):
+        self.out_file.write("@LCL\nD=M\n@R13\nM=D\n") # R13 (FRAME) = LCL
+        self.out_file.write("@5\nA=D-A\nD=M\n@R14\nM=D\n") # R14 (RET) = *(FRAME - 5)
+        self.out_file.write("@SP\nAM=M-1\nD=M\n@ARG\nA=M\nM=D\n") # *ARG = pop()
+        self.out_file.write("@ARG\nD=M+1\n@SP\nM=D\n") # SP = ARG + 1
+        for seg in ["THAT", "THIS", "ARG", "LCL"]:
+            self.out_file.write(f"@R13\nAM=M-1\nD=M\n@{seg}\nM=D\n")
+        self.out_file.write("@R14\nA=M\n0;JMP\n") # goto RET
+
+    def write_function(self, function_name, num_locals):
+        self.write_label(function_name)
+        for _ in range(num_locals):
+            self.out_file.write("@SP\nA=M\nM=0\n@SP\nM=M+1\n")
 
     def write_arithmetic(self, command):
-        if command == "add":
-            self.out_printer.write(self.arithmetic_template1() + "M=M+D\n")
-        elif command == "sub":
-            self.out_printer.write(self.arithmetic_template1() + "M=M-D\n")
-        elif command == "and":
-            self.out_printer.write(self.arithmetic_template1() + "M=M&D\n")
-        elif command == "or":
-            self.out_printer.write(self.arithmetic_template1() + "M=M|D\n")
-        elif command == "gt":
-            self.out_printer.write(self.arithmetic_template2("JLE"))
-            self.arth_jump_flag += 1
-        elif command == "lt":
-            self.out_printer.write(self.arithmetic_template2("JGE"))
-            self.arth_jump_flag += 1
-        elif command == "eq":
-            self.out_printer.write(self.arithmetic_template2("JNE"))
-            self.arth_jump_flag += 1
-        elif command == "not":
-            self.out_printer.write("@SP\nA=M-1\nM=!M\n")
-        elif command == "neg":
-            self.out_printer.write("D=0\n@SP\nA=M-1\nM=D-M\n")
+        if command == "neg": code = "M=-M\n"
+        elif command == "not": code = "M=!M\n"
         else:
-            raise ValueError("Invalid arithmetic command")
+            code = "@SP\nAM=M-1\nD=M\nA=A-1\n"
+            if command == "add": code += "M=M+D\n"
+            elif command == "sub": code += "M=M-D\n"
+            elif command == "and": code += "M=M&D\n"
+            elif command == "or": code += "M=M|D\n"
+            elif command in {"eq", "gt", "lt"}:
+                jump_map = {"eq": "JEQ", "gt": "JGT", "lt": "JLT"}
+                label = f"COMP.{self.arth_jump_count}"
+                self.arth_jump_count += 1
+                code += (f"D=M-D\n@{label}.TRUE\nD;{jump_map[command]}\n"
+                         f"@SP\nA=M-1\nM=0\n@{label}.END\n0;JMP\n"
+                         f"({label}.TRUE)\n@SP\nA=M-1\nM=-1\n({label}.END)\n")
+        self.out_file.write(code)
 
     def write_push_pop(self, command, segment, index):
         if command == Parser.PUSH:
             if segment == "constant":
-                self.out_printer.write(f"@{index}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
-            elif segment in ["local", "argument", "this", "that"]:
-                base = {"local": "LCL", "argument": "ARG", "this": "THIS", "that": "THAT"}[segment]
-                self.out_printer.write(self.push_template1(base, index, False))
-            elif segment == "temp":
-                self.out_printer.write(self.push_template1("R5", index + 5, False))
-            elif segment == "pointer" and index in (0, 1):
-                self.out_printer.write(self.push_template1("THIS" if index == 0 else "THAT", index, True))
-            elif segment == "static":
-                self.out_printer.write(f"@{self.file_name}{index}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+                self.out_file.write(f"@{index}\nD=A\n")
+            else:
+                base_map = {"local": "LCL", "argument": "ARG", "this": "THIS", "that": "THAT"}
+                if segment in base_map: # Indirect: local, argument, this, that
+                    self.out_file.write(f"@{base_map[segment]}\nD=M\n@{index}\nA=D+A\nD=M\n")
+                elif segment == "pointer": # Direct: pointer, temp, static
+                    self.out_file.write(f"@{'THIS' if index == 0 else 'THAT'}\nD=M\n")
+                elif segment == "temp":
+                    self.out_file.write(f"@R{5 + index}\nD=M\n")
+                elif segment == "static":
+                    self.out_file.write(f"@{self.file_name}.{index}\nD=M\n")
+            self.out_file.write("@SP\nA=M\nM=D\n@SP\nM=M+1\n")
         elif command == Parser.POP:
-            if segment in ["local", "argument", "this", "that"]:
-                base = {"local": "LCL", "argument": "ARG", "this": "THIS", "that": "THAT"}[segment]
-                self.out_printer.write(self.pop_template1(base, index, False))
+            base_map = {"local": "LCL", "argument": "ARG", "this": "THIS", "that": "THAT"}
+            if segment in base_map: # Indirect: local, argument, this, that
+                self.out_file.write(f"@{base_map[segment]}\nD=M\n@{index}\nD=D+A\n@R13\nM=D\n")
+            elif segment == "pointer": # Direct: pointer, temp, static
+                self.out_file.write(f"@{('THIS' if index == 0 else 'THAT')}\nD=A\n@R13\nM=D\n")
             elif segment == "temp":
-                self.out_printer.write(self.pop_template1("R5", index + 5, False))
-            elif segment == "pointer" and index in (0, 1):
-                self.out_printer.write(self.pop_template1("THIS" if index == 0 else "THAT", index, True))
+                self.out_file.write(f"@R{5 + index}\nD=A\n@R13\nM=D\n")
             elif segment == "static":
-                self.out_printer.write(f"@{self.file_name}{index}\nD=A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n")
-        else:
-            raise ValueError("Invalid push/pop command")
-
-    def write_label(self, label):
-        if re.match(r"^[^0-9][0-9A-Za-z_:.\$]+$", label):
-            self.out_printer.write(f"({label})\n")
-        else:
-            raise ValueError("Invalid label format")
-
-    def write_goto(self, label):
-        if re.match(r"^[^0-9][0-9A-Za-z_:.\$]+$", label):
-            self.out_printer.write(f"@{label}\n0;JMP\n")
-        else:
-            raise ValueError("Invalid label format")
-
-    def write_if(self, label):
-        if re.match(r"^[^0-9][0-9A-Za-z_:.\$]+$", label):
-            self.out_printer.write(self.arithmetic_template1() + f"@{label}\nD;JNE\n")
-        else:
-            raise ValueError("Invalid label format")
-
-    def write_init(self):
-        self.out_printer.write("@256\nD=A\n@SP\nM=D\n")
-        self.write_call("Sys.init", 0)
-
-    def write_call(self, function_name, num_args):
-        label = f"RETURN_LABEL{CodeWriter.label_cnt}"
-        CodeWriter.label_cnt += 1
-        self.out_printer.write(f"@{label}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
-        for segment in ["LCL", "ARG", "THIS", "THAT"]:
-            self.out_printer.write(self.push_template1(segment, 0, True))
-        self.out_printer.write(f"@SP\nD=M\n@5\nD=D-A\n@{num_args}\nD=D-A\n@ARG\nM=D\n")
-        self.out_printer.write("@SP\nD=M\n@LCL\nM=D\n")
-        self.out_printer.write(f"@{function_name}\n0;JMP\n({label})\n")
-
-    def write_return(self):
-        self.out_printer.write(self.return_template())
-
-    def write_function(self, function_name, num_locals):
-        self.out_printer.write(f"({function_name})\n")
-        for _ in range(num_locals):
-            self.write_push_pop(Parser.PUSH, "constant", 0)
-
-    def return_template(self):
-        return ("@LCL\nD=M\n@R11\nM=D\n@5\nA=D-A\nD=M\n@R12\nM=D\n" +
-                self.pop_template1("ARG", 0, False) +
-                "@ARG\nD=M\n@SP\nM=D+1\n" +
-                self.pre_frame_template("THAT") +
-                self.pre_frame_template("THIS") +
-                self.pre_frame_template("ARG") +
-                self.pre_frame_template("LCL") +
-                "@R12\nA=M\n0;JMP\n")
-
-    def pre_frame_template(self, position):
-        return f"@R11\nD=M-1\nAM=D\nD=M\n@{position}\nM=D\n"
-
-    def arithmetic_template1(self):
-        return "@SP\nAM=M-1\nD=M\nA=A-1\n"
-
-    def arithmetic_template2(self, jump_type):
-        label = self.arth_jump_flag
-        return ("@SP\nAM=M-1\nD=M\nA=A-1\nD=M-D\n" +
-                f"@FALSE{label}\nD;{jump_type}\n" +
-                "@SP\nA=M-1\nM=-1\n" +
-                f"@CONTINUE{label}\n0;JMP\n" +
-                f"(FALSE{label})\n@SP\nA=M-1\nM=0\n(CONTINUE{label})\n")
-
-    def push_template1(self, segment, index, is_direct):
-        code = "" if is_direct else f"@{index}\nA=D+A\nD=M\n"
-        return f"@{segment}\nD=M\n{code}@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-
-    def pop_template1(self, segment, index, is_direct):
-        code = "D=A\n" if is_direct else f"D=M\n@{index}\nD=D+A\n"
-        return f"@{segment}\n{code}@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n"
+                self.out_file.write(f"@{self.file_name}.{index}\nD=A\n@R13\nM=D\n")
+            self.out_file.write("@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n")
 
     def close(self):
-        self.out_printer.close()
+        self.out_file.close()
--- projects/08/vm_translator_v2/parser.py
+++ projects/08/vm_translator_v2/parser.py
@@ -1,3 +1,5 @@
+import os
+
 class Parser:
     ARITHMETIC = 0
     PUSH = 1
@@ -8,30 +10,24 @@ class Parser:
     FUNCTION = 6
     RETURN = 7
     CALL = 8
-
-    arithmetic_cmds = [
-        "add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"
-    ]
+    ARITHMETIC_CMDS = {"add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"}
 
     def __init__(self, file_in):
-        self.cmds = []
-        self.current_cmd = ""
-        self.arg_type = -1
-        self.argument1 = ""
-        self.argument2 = -1
-
         try:
             with open(file_in, "r") as f:
-                lines = f.readlines()
-                preprocessed = [
-                    self.no_comments(line).strip()
-                    for line in lines
-                    if self.no_comments(line).strip()
+                self.cmds = [
+                    line.split("//")[0].strip()
+                    for line in f
+                    if line.split("//")[0].strip()
                 ]
-                self.cmds = preprocessed
-                self.index = 0
+            self.index = 0
+            self.current_cmd = ""
+            self.arg_type = -1
+            self.argument1 = ""
+            self.argument2 = -1
         except FileNotFoundError:
-            print("File not found!")
+            print(f"File not found: {file_in}")
+            self.cmds = []
 
     def has_more_commands(self):
         return self.index < len(self.cmds)
@@ -39,73 +35,38 @@ class Parser:
     def advance(self):
         self.current_cmd = self.cmds[self.index]
         self.index += 1
-        self.argument1 = ""
-        self.argument2 = -1
-
         segs = self.current_cmd.split()
+        command = segs[0]
 
-        if len(segs) > 3:
-            raise ValueError("Too many arguments!")
-
-        if segs[0] in self.arithmetic_cmds:
+        if command in self.ARITHMETIC_CMDS:
             self.arg_type = self.ARITHMETIC
-            self.argument1 = segs[0]
-        elif segs[0] == "return":
+            self.argument1 = command
+        elif command == "return":
             self.arg_type = self.RETURN
-            self.argument1 = segs[0]
         else:
+            type_map = {
+                "push": self.PUSH, "pop": self.POP, "label": self.LABEL,
+                "if-goto": self.IF, "goto": self.GOTO, "function": self.FUNCTION,
+                "call": self.CALL
+            }
+            self.arg_type = type_map[command]
             self.argument1 = segs[1]
-            if segs[0] == "push":
-                self.arg_type = self.PUSH
-            elif segs[0] == "pop":
-                self.arg_type = self.POP
-            elif segs[0] == "label":
-                self.arg_type = self.LABEL
-            elif segs[0] == "if-goto":
-                self.arg_type = self.IF
-            elif segs[0] == "goto":
-                self.arg_type = self.GOTO
-            elif segs[0] == "function":
-                self.arg_type = self.FUNCTION
-            elif segs[0] == "call":
-                self.arg_type = self.CALL
-            else:
-                raise ValueError("Unknown command type")
-
             if self.arg_type in {self.PUSH, self.POP, self.FUNCTION, self.CALL}:
-                try:
-                    self.argument2 = int(segs[2])
-                except:
-                    raise ValueError("Argument2 must be an integer")
+                self.argument2 = int(segs[2])
 
     def command_type(self):
-        if self.arg_type != -1:
-            return self.arg_type
-        else:
-            raise RuntimeError("No command")
+        return self.arg_type
 
     def arg1(self):
-        if self.command_type() != self.RETURN:
-            return self.argument1
-        else:
-            raise RuntimeError("arg1 should not be called for RETURN command")
+        if self.command_type() == self.RETURN:
+            raise RuntimeError("arg1() should not be called for RETURN command")
+        return self.argument1
 
     def arg2(self):
-        if self.command_type() in {self.PUSH, self.POP, self.FUNCTION, self.CALL}:
-            return self.argument2
-        else:
-            raise RuntimeError("arg2 not available for this command type")
+        if self.command_type() not in {self.PUSH, self.POP, self.FUNCTION, self.CALL}:
+            raise RuntimeError("arg2() not available for this command type")
+        return self.argument2
 
     @staticmethod
     def no_comments(line):
         return line.split("//")[0]
-
-    @staticmethod
-    def no_spaces(s):
-        return ''.join(s.split())
-
-    @staticmethod
-    def get_ext(filename):
-        if '.' in filename:
-            return filename[filename.rindex('.'):]
-        return ''
